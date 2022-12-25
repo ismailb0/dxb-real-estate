8dc41048a475ff88265189daa3884a1187d1c59e
Ismail Benmoussa <ismailbenmoussa@gmail.com>
2022-12-25 17:15:32 +0400

    Update overlay styling

--- src/AreaOverlay/AreaOverlay.js
+++ src/AreaOverlay/AreaOverlay.js
@@ -1,7 +1,19 @@
 import { Polygon } from '@react-google-maps/api';
 
 function AreaOverlay(props) {
-  return <Polygon paths={props.areaCoordinates} />
+
+  const defaultOptions = {
+      fillColor: '#FF0000',
+      fillOpacity: 0.1,
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.1,
+      strokeWeight: 8
+  };
+
+  return <Polygon
+            paths={props.areaCoordinates}
+            options={defaultOptions}
+          />
 }
 
 export default AreaOverlay;
