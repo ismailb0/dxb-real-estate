f55238d7d8dbe84bae80f07e722a1f851b33837a
Ismail Benmoussa <ismailbenmoussa@gmail.com>
2022-12-25 17:36:37 +0400

    Highlight area on hover

--- src/AreaOverlay/AreaOverlay.js
+++ src/AreaOverlay/AreaOverlay.js
@@ -1,19 +1,44 @@
+import React, { Component } from 'react';
 import { Polygon } from '@react-google-maps/api';
 
-function AreaOverlay(props) {
+class AreaOverlay extends Component {
 
-  const defaultOptions = {
+  defaultOptions = {
       fillColor: '#FF0000',
       fillOpacity: 0.1,
       strokeColor: '#FF0000',
       strokeOpacity: 0.1,
       strokeWeight: 8
-  };
+  }
 
-  return <Polygon
-            paths={props.areaCoordinates}
-            options={defaultOptions}
-          />
+  state = { options: this.defaultOptions }
+
+  handleMouseOver = () => {
+    this.setState({
+      options: {
+          fillColor: '#FF0000',
+          fillOpacity: 1,
+          strokeColor: '#FF0000',
+          strokeOpacity: 0.1,
+          strokeWeight: 8
+      }
+    })
+  }
+
+  handleMouseOut = () => {
+    this.setState({ options: this.defaultOptions })
+  }
+
+  render() {
+    return (
+      <Polygon
+        paths={this.props.areaCoordinates}
+        options={this.state.options}
+        onMouseOver={this.handleMouseOver}
+        onMouseOut={this.handleMouseOut}
+      />
+    );
+  }
 }
 
 export default AreaOverlay;
